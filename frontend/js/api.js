fdfcf9a4e61da8223a8ebbe2abfb05e147a37f3c
JuanTrillini06 <juanjose_207@hotmail.com>
2026-08-13 21:30:23 -0300

    API de back corregida

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 // Configuración global para fetch API
-const API_URL = process.env.API_URL;
+const API_URL = 'https://cuentas-personales.onrender.com';
 
 // Obtener token
 const getToken = () => localStorage.getItem('token');
